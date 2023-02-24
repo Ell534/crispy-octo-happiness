9423971bba08e231152adf048863d96a2a003b3a
Ell534 <elliot94b@gmail.com>
2023-02-24 14:37:17 +0000

    update test file to include comment count on expected object line 145

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -142,6 +142,7 @@ describe('/api/reviews/:review_id', () => {
             category: 'euro game',
             owner: 'mallionaire',
             created_at: '2021-01-18T10:00:20.514Z',
+            comment_count: expect.any(Number),
           },
         ]);
       });
